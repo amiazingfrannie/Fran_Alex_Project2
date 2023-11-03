6e3a3eb6c52f6068ea1be88366934b1946df7a3b
Alex Dickey <alexander.c.dickey@gmail.com>
2023-11-03 15:00:54 -0700

    updated compareHighlight

--- src/gameUtil.jsx
+++ src/gameUtil.jsx
@@ -8,24 +8,34 @@ export function checkGuess(userInput, chosenWord) {
 
 export const compareHighlight = (userInput, chosenWord) => {
 
-    // const colors = ['yellow', 'green', 'gray'];
+    const colors = ['yellow', 'green'];
     let unmatchedLetterInChosen = [...chosenWord];
+    console.log(typeof(unmatchedLetterInChosen), unmatchedLetterInChosen)
+    let compareColors = Array(unmatchedLetterInChosen.length).fill('gray');
 
     console.log("userInput:", userInput);
     console.log("chosenWord:", chosenWord); 
 
-    return userInput.map((letter, index) => {
-        if (letter == chosenWord[index]) {
-            unmatchedLetterInChosen[index] = null;
-            return 'green';
+    for (let i = 0; i< colors.length; i++) {
+        if (i == 0) {
+            userInput.map((letter, index) => {
+                if (letter == unmatchedLetterInChosen[index]) {
+                    unmatchedLetterInChosen[index] = null;
+                    compareColors[index] = 'green';
+                }
+            });
+            console.log(compareColors)
+        } else {
+            userInput.map((letter, index) => {
+                if (compareColors[index] != 'green') {
+                    let matchIdx = unmatchedLetterInChosen.indexOf(letter);
+                    if (matchIdx !== -1 ) {
+                        unmatchedLetterInChosen[matchIdx] = null;
+                        compareColors[index] = 'yellow';
+                    }
+                }
+            });
         }
-
-        const matchIdx = unmatchedLetterInChosen.indexOf(letter);
-        if (matchIdx !== -1 && matchIdx < index) {
-            unmatchedLetterInChosen[matchIdx] = null;
-            return 'yellow';
-            }
-        return 'gray';
-    });
+    }
+    return  compareColors;
 }
-  
